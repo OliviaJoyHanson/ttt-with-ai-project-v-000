3b34dfe838b63eec5c663c8549d864b03a2b94cb
Olivia Hanson <olivia.joy.hanson@gmail.com>
2020-11-19 04:06:08 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -13,15 +13,15 @@ class Players::Computer < Player
   #  elsif board.turn_count == 2 && board.taken?("5") && [1,3,7,9].all?{|cell| !board.taken?(cell.to_s)}
 	 #		move = [1,3,7,9].sample.to_s
     elsif board.turn_count == 3 && board.turn_count < 7 && board.taken?("5") && ([1,3,7,9].select{|cell| board.position("#{cell}") == self.token})
-      if board.position("1") == self.token 
+      if board.position("1") == self.token
         move = [3,7].sample
       elsif board.position("3") == self.token
-        move = [1,9].sample 
-      elsif board.position("7") == self.token 
-        move = [1,9].sample 
-      elsif board.position("9") == self.token 
+        move = [1,9].sample
+      elsif board.position("7") == self.token
+        move = [1,9].sample
+      elsif board.position("9") == self.token
         move = [3,7].samepl
-      end 
+      end
     elsif (board.turn_count > 3 && board.turn_count < 7 && ((board.position("1") != " " && board.position("2") != " " && board.position("1") == board.position("2") && !board.taken?("3")) || (board.position("2") != " " && board.position("3") != " " && board.position("2") == board.position("3") && !board.taken?("1
     ")) || (board.position("4") != " " && board.position("5") != " " && board.position("4") == board.position("5") && !board.taken?("6")) || (board.position("5") != " " && board.position("6") != " " && board.position("5") == board.position("6") && !board.taken?("4")) || (board.position("7") != " " && board.position("8") != " " && board.position("7") == board.position("8") && !board.taken?("9")) || (board.position("8") != " " && board.position("9") != " " && board.position("8") == board.position("9") && !board.taken?("7")) || (board.position("1") != " " && board.position("4") != " " && board.position("1") == board.position("4") && !board.taken?("7")) || (board.position("4") != " " && board.position("7") != " " && board.position("4") == board.position("7") && !board.taken?("1")) || (board.position("2") != " " && board.position("5") != " " && board.position("2") == board.position("5") && !board.taken?("8")) || (board.position("5") != " " && board.position("8") != " " && board.position("5") == board.position("8") && !board.taken?("2")) || (board.position("3") != " " && board.position("6") != " " && board.position("3") == board.position("6") && !board.taken?("9")) || (board.position("6") != " " && board.position("9") != " " && board.position("6") == board.position("9") && !board.taken?("3")) || (board.position("1") != " " && board.position("5") != " " && board.position("1") == board.position("5") && !board.taken?("9")) || (board.position("5") != " " && board.position("9") != " " && board.position("5") == board.position("9") && !board.taken?("1")) || (board.position("7") != " " && board.position("5") != " " && board.position("7") == board.position("5") && !board.taken?("3")) || (board.position("5") != " " && board.position("3") != " " && board.position("5") == board.position("3") && !board.taken?("7")))) || (board.turn_count > 3 && board.turn_count < 7 && ((board.position("1") != " " && board.position("3") != " " && board.position("1") == board.position("3") && !board.taken?("2")) || (board.position("7") != " " && board.position("9") != " " && board.position("7") == board.position("9") && !board.taken?("8")) || (board.position("1") != " " && board.position("7") != " " && board.position("1") == board.position("7") && !board.taken?("4")) || (board.position("3") != " " && board.position("9") != " " && board.position("3") == board.position("9") && !board.taken?("6"))))
       if board.position("1") != " " && board.position("2") != " " && board.position("1") == board.position("2") && !board.taken?("3")
