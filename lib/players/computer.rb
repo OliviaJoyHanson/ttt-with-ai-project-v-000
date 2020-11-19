798da4291d396d42a1faea6ea4583ce92a4e3ee3
Olivia Hanson <olivia.joy.hanson@gmail.com>
2020-11-19 04:15:46 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -86,7 +86,7 @@ class Players::Computer < Player
       elsif board.position("9") == self.token && !board.taken?("1")
         move = "1"
       end
-    elsif board.turn_count >=7
+    elsif board.turn_count >= 3
       move = [1,2,3,4,5,6,7,8,9].detect{|cell| board.taken?(cell.to_s) == false}.to_s
 		end
     move
